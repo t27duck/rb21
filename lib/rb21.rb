d53fa809774e0e6b23b4fecd8884ccec01e69ee5
Tony Drake <t27duck@gmail.com>
2018-09-29 11:06:39 -0400

    Rubocop configuration and autolinting

--- lib/rb21.rb
+++ lib/rb21.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "rb21/version"
 
 module Rb21
--- spec/rb21_spec.rb
+++ spec/rb21_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.describe Rb21 do
   it "has a version number" do
     expect(Rb21::VERSION).not_to be nil
